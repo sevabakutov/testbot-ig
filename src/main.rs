3a171bfa698773c3c2bf909fba8af4fb141806f8
Roman Kolosov <romankolosov@Mac-mini-Roman.local>
2025-07-07 12:02:19 +0200

    changed host

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
-use std::env;
+    use std::env;
 
-use actix_web::{http::StatusCode, web, App, HttpRequest, HttpResponse, HttpServer};
+    use actix_web::{http::StatusCode, web, App, HttpRequest, HttpResponse, HttpServer};
 
-async fn receive_messages(req: HttpRequest) -> HttpResponse {
-    println!("Received HttpRequest: {:?}", req);
-    HttpResponse::new(StatusCode::OK)
-}
+    async fn receive_messages(req: HttpRequest) -> HttpResponse {
+        println!("Received HttpRequest: {:?}", req);
+        HttpResponse::new(StatusCode::OK)
+    }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let port = env::var("PORT")
-        .unwrap_or_else(|_| "3000".to_string())
-        .parse()
-        .expect("PORT must be a number");
+    #[actix_web::main]
+    async fn main() -> std::io::Result<()> {
+        let port = env::var("PORT")
+            .unwrap_or_else(|_| "3000".to_string())
+            .parse()
+            .expect("PORT must be a number");
 
-    HttpServer::new(|| {
-        App::new()
-            .service(
-                web::scope("/app")
-                    .route("/receive_messages", web::post().to(receive_messages))
-            )
-    })
-    .bind(("127.0.0.1", port))?
-    .run()
-    .await
-}
+        HttpServer::new(|| {
+            App::new()
+                .service(
+                    web::scope("/app")
+                        .route("/receive_messages", web::post().to(receive_messages))
+                )
+        })
+        .bind(("0.0.0.0", port))?
+        .run()
+        .await
+    }
