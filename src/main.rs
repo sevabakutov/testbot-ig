f4e190eca213c2295561d5e46daca1342abe8533
Roman Kolosov <romankolosov@Mac-mini-Roman.local>
2025-07-07 11:24:26 +0200

    added a simple server and a simple handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{http::StatusCode, web, App, HttpRequest, HttpResponse, HttpServer};
+
+async fn receive_messages(req: HttpRequest) -> HttpResponse {
+    println!("Received HttpRequest: {:?}", req);
+    HttpResponse::new(StatusCode::OK)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::scope("/app")
+                    .route("/receive_messages", web::post().to(receive_messages))
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
